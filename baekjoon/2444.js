6f8d3234dd573786d827b1917a8ecdd264dcbfc7
이제훈 <2eejehoon@gmail.com>
2024-01-31 00:08:13 +0900

    10988

--- baekjoon/2444.js
+++ baekjoon/2444.js
@@ -1,4 +1,12 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const str = fs.readFileSync(filePath).toString().trim();
-console.log(str);
+const N = fs.readFileSync(filePath).toString().trim();
+const a = [];
+for (let i = 0; i < N; i++) {
+  let temp = " ".repeat(N - 1 - i) + "*".repeat(2 * i + 1);
+  a.push(temp);
+}
+for (let j = N - 2; j >= 0; j--) {
+  a.push(a[j]);
+}
+console.log(a.join("\n"));
