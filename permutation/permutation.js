400da5ffbe739adc1b31775e4d232b049ad56c56
이제훈 <2eejehoon@gmail.com>
2024-01-30 22:58:52 +0900

    permutation

---
+++ permutation/permutation.js
@@ -0,0 +1,23 @@
+function solution(arr) {
+  const answer = [];
+  const visit = Array(arr.length).fill(false);
+  const dfs = (depth, path) => {
+    answer.push(path);
+    if (depth === arr.length) {
+      return;
+    }
+
+    for (let i = 0; i < visit.length; i++) {
+      if (!visit[i]) {
+        visit[i] = true;
+        dfs(depth + 1, [...path, arr[i]]);
+        visit[i] = false;
+      }
+    }
+  };
+  dfs(0, []);
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3]));
