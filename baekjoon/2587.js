96477c41dd79c7fc17c78e2de1cf66306bad6e51
이제훈 <2eejehoon@gmail.com>
2024-02-07 21:53:48 +0900

    2587

--- baekjoon/2587.js
+++ baekjoon/2587.js
@@ -1,3 +1,10 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const arr = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
+
+arr.sort((a, b) => a - b);
+const sum = arr.reduce((acc, cur) => acc + cur, 0);
+const average = sum / arr.length;
+const median = arr[2];
+console.log(average);
+console.log(median);
