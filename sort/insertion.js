2ff702ec176421f2fd038aef153905d7dad55ae3
이제훈 <2eejehoon@gmail.com>
2024-02-07 21:38:45 +0900

    삽입 정렬 수정

--- sort/insertion.js
+++ sort/insertion.js
@@ -1,10 +1,7 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let value = arr[i];
-    for (let j = i - 1; j >= 0; j--) {
-      if (value > arr[j]) {
-        continue;
-      }
+    for (let j = i - 1; j >= 0 && value < arr[j]; j--) {
       arr[j + 1] = arr[j];
       arr[j] = value;
     }
