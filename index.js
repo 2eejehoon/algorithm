a98f31ed6d579c9325854437e1091bd139259dd3
이제훈 <2eejehoon@gmail.com>
2024-02-14 09:12:44 +0900

    src 디렉토리 추가

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().trim();
-const set = new Set();
-for (let i = 0; i < input.length; i++) {
-  let temp = "";
-  for (let j = i; j < input.length; j++) {
-    temp += input[j];
-    set.add(temp);
-  }
-}
-console.log(set.size);
+const [T, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+const nums = arr.map((v) => v.split(" ").map(Number));
+const answer = [];
+console.log(answer.join("\n"));
