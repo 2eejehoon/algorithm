d1e0b7fe5f0fcd8f7e32a38935c186bd446ce620
이제훈 <2eejehoon@gmail.com>
2024-02-20 17:30:24 +0900

    Deque 자료구조 추가

---
+++ src/Deque.js
@@ -0,0 +1,70 @@
+class Node {
+  constructor(value, index) {
+    this.value = value;
+    this.index = index;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class Deque {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  unshift(value, index) {
+    const node = new Node(value, index);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      const head = this.head;
+      this.head = node;
+      this.head.next = head;
+      head.prev = this.head;
+    }
+    this.length++;
+  }
+
+  push(value, index) {
+    const node = new Node(value, index);
+    if (this.length === 0) {
+      this.tail = node;
+      this.head = node;
+    } else {
+      const tail = this.tail;
+      this.tail = node;
+      this.tail.prev = tail;
+      tail.next = this.tail;
+    }
+    this.length++;
+  }
+
+  shift() {
+    const head = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = head.next;
+      this.head.prev = null;
+    }
+    this.length--;
+    return head;
+  }
+
+  pop() {
+    const tail = this.tail;
+    if (this.length === 1) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.tail = tail.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return tail;
+  }
+}
