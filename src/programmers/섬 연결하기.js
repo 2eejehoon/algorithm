a849f5f9c4c0c58a6f247b7a56e2e05fe9d467c7
이제훈 <2eejehoon@gmail.com>
2024-02-27 14:12:37 +0900

    섬 연결하기기

---
+++ src/programmers/섬 연결하기.js
@@ -0,0 +1,38 @@
+function solution(n, costs) {
+  const unionParent = (set, a, b) => {
+    a = getParent(set, a);
+    b = getParent(set, b);
+    if (a < b) {
+      set[b] = a;
+    } else {
+      set[a] = b;
+    }
+  };
+
+  const hasEquelParent = (set, a, b) => {
+    a = getParent(set, a);
+    b = getParent(set, b);
+    if (a === b) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const getParent = (set, x) => {
+    if (set[x] === x) {
+      return x;
+    }
+    return getParent(set, set[x]);
+  };
+
+  let answer = 0;
+  const table = Array.from({ length: n }, (_, i) => i);
+  for (let [a, b, cost] of costs) {
+    if (!hasEquelParent(table, a, b)) {
+      answer += cost;
+      unionParent(table, a, b);
+    }
+  }
+  return answer;
+}
