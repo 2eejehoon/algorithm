ab0010d1535a0a6f927998b8649c4007a706079a
이제훈 <2eejehoon@gmail.com>
2024-03-08 09:30:31 +0900

    섬연결하기 수정

--- src/programmers/섬 연결하기.js
+++ src/programmers/섬 연결하기.js
@@ -27,6 +27,7 @@ function solution(n, costs) {
   };
 
   let answer = 0;
+  costs.sort((a, b) => a[2] - b[2]);
   const table = Array.from({ length: n }, (_, i) => i);
   for (let [a, b, cost] of costs) {
     if (!hasEquelParent(table, a, b)) {
