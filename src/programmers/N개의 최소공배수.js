6ba4fa544b9d3eda875d591c70c1c7264f6d016f
이제훈 <2eejehoon@gmail.com>
2024-03-21 19:02:55 +0900

    N개의 최소공배수

---
+++ src/programmers/N개의 최소공배수.js
@@ -0,0 +1,14 @@
+const getGcd = (num1, num2) => {
+    return num2 > 0 ? getGcd(num2, num1 % num2) : num1
+}
+const getLcm = (num1, num2) => {
+    return (num1 * num2) / getGcd(num1, num2)
+}
+
+function solution(arr) {
+    let answer = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        answer = getLcm(answer, arr[i])
+    }
+    return answer
+}
