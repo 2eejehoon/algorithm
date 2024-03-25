aaef7c2f651b6afb5f98cb7cb15ed931eb2c452c
이제훈 <2eejehoon@gmail.com>
2024-03-25 18:20:46 +0900

    귤 고르기

---
+++ src/programmers/귤 고르기.js
@@ -0,0 +1,22 @@
+function solution(k, tangerine) {
+  const obj = {};
+  for (let x of tangerine) {
+    if (x in obj) {
+      obj[x]++;
+    } else {
+      obj[x] = 1;
+    }
+  }
+  const arr = Object.entries(obj).map((v) => [v[0], v[1]]);
+  arr.sort((a, b) => b[1] - a[1]);
+  let answer = 0;
+  let sum = 0;
+  for (let [key, value] of arr) {
+    sum += value;
+    answer++;
+    if (sum >= k) {
+      break;
+    }
+  }
+  return answer;
+}
